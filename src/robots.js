1d3ea402835e5e514f11d5576c6d8d2f2b4a67f2
Deimant <d.savickas49@yahoo.com>
2020-09-08 03:07:02 +0300

    pushing roboFriends

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+{
+    id: 1,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'Sincere@april.biz'
+},
+{
+    id: 2,
+    name: 'Ervin Howell',
+    username: 'Antonette',
+    email: 'Shanna@melissa.tv'
+},
+{
+    id: 3,
+    name: 'Clementine Bauch',
+    username: 'Samantha',
+    email: 'Nathan@yeasenia.net'
+},
+{
+    id: 4,
+    name: 'Patricia Lebsack',
+    username: 'Karianne',
+    email: 'Julianne.OConner@koory.org'
+},
+{
+    id: 5,
+    name: 'Chelsey Dietrich',
+    username: 'Kamren',
+    email: 'Lucio_Hettinger@annie.ca'
+},
+{
+    id: 6,
+    name: 'Mrs. Dennis Schulist',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info'
+},
+{
+    id: 7,
+    name: 'Kurtis Weissant',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoager@billy.biz'
+},
+{
+    id: 8,
+    name: 'Nicholas Runolfsdottir V',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamand.me'
+},
+{
+    id: 9,
+    name: 'Glena Reichert',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+},
+{
+    id: 10,
+    name: 'Clementina DuBuque',
+    username: 'Moriah.Stantion',
+    email: 'Rey.Padberg@karina.biz'
+},
+];
